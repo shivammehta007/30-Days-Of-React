4c5b31a860b1b43d2405639eaa55f7de639479c9
Shivam Mehta <shivam.mehta007@gmail.com>
2022-08-03 15:36:55 +0200

    Day 4

---
+++ solutions/day-04/testapp/src/index.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Header Component
+
+
+const welcome = 'Welcome to 30 Days Of React Challenge'
+const title = 'Getting Started React'
+const subtitle = 'JavaScript Library'
+const author = {
+  firstName: 'Asabeneh',
+  lastName: 'Yetayeh',
+}
+const date = 'Oct 2, 2020'
+
+
+const Header = () => (
+  <header>
+    <div className='header-wrapper'>
+      <h1>{welcome}</h1>
+      <h2>{title}</h2>
+      <h3>{subtitle}</h3>
+      <p>
+        Instructor: {author.firstName} {author.lastName}
+      </p>
+      <small>Date: {date}</small>
+    </div>
+  </header>
+)
+
+
+// TechList Component
+const TechList = () => {
+  const techs = ['HTML', 'CSS', 'JavaScript']
+  const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
+  return techsFormatted
+}
+
+
+
+// Main Component
+const Main = () => (
+  <main>
+    <div className='main-wrapper'>
+      <p>Prerequisite to get started react.js:</p>
+      <ul>
+        <TechList />
+        <Button /><Button /><Button /><Button /><Button /><Button />
+      </ul>
+    </div>
+  </main>
+)
+
+// Footer Component
+const Footer = () => (
+  <footer>
+    <div className='footer-wrapper'>
+      <p>Copyright 2020</p>
+    </div>
+  </footer>
+)
+
+
+const hexaColor = () => {
+  let str = '0123456789abcdef'
+  let color = ''
+  for (let i = 0; i < 6; i++) {
+    let index = Math.floor(Math.random() * str.length)
+    color += str[index]
+  }
+  return '#' + color
+}
+
+const Button = () => <button style={{
+  padding: '10px 20px',
+  background: hexaColor(),
+  border: 'none',
+  borderRadius: 5,
+}}> action </button>
+
+
+const App = () => (
+  <div className='app'>
+    <Header />
+    {hexaColor()}
+    <Main />
+    <Footer />
+  </div>
+)
+
+root.render(<App />);
